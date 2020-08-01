96274f2ade1377aee669f6e57247293c30dbe977
Basanta <raibasant72000@gmail.com>
2020-08-01 18:35:12 -0400

    changed in test.js

--- src/components/test.js
+++ src/components/test.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Test =() =>{
 
 
-    const names = ['Adhrit is a good boy','Robot ','Aeroplane Army']
+    const names = ['ADhrit is a smart boy','Robot ','Aeroplane Army']
     const nameslist = names.map(name => 
         <h2 style={{color:"red"}}> {name} </h2>
     )
